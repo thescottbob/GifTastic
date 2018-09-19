ca177e0d868ca71799d0a73833d8111d2b2689ef
thescottbob <scottmcmains@gmail.com>
2018-09-19 11:54:01 -0700

    GifTastic

---
+++ javascript.js
@@ -0,0 +1,8 @@
+//Declare a variable called buttonArray and set it equal to an array of strings
+var buttonArray = ["Superman", "Batman", "Wonder Woman", "The Green Lantern", "Iron Man"];
+
+//use .push to add elements to the array
+
+//Create an array of buttons
+$('#buttonArray').append(<button>buttonArray</button>);
+
